e19f8bbefb9e14ea88860388fd722d8021ec265c
Maccio92 <valerio362@gmail.com>
2022-01-17 14:52:49 +0100

    add Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="container-fluid px-5">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="">
+</div>
+</template>
+
+<script>
+export default {
+name: 'Header',
+}
+</script>
+
+<style lang="scss" scoped>
+div{
+    height: 80px;
+    background-color: #2e3a46;
+    display: flex;
+    align-items: center;
+    img{
+        width: 50px;
+    }
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+<div></div>
+</template>
+
+<script>
+// import axios from 'axios'
+export default {
+name: 'Main',
+data(){
+    return{
+    queryApi: 'https://flynn.boolean.careers/exercises/api/array/music',
+    }
+}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
