e661cd2a1e42f05e42a708788f04ad5ba3c9683a
Maccio92 <valerio362@gmail.com>
2022-01-18 19:03:36 +0100

    add refactoring

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -76,11 +76,12 @@ methods: {
 <style lang="scss" >
 .bg{
     background-color: #1e2d3b;
-    height: calc(100% - 80px);
+    height: calc(100vh - 80px);
 }
 h1{
     color: white;
     text-transform: uppercase;
     text-align: center;
+
 }
 </style>
